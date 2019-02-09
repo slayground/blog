f095d484280c32d4fc2d193c7af94051ba7642ab
Huy Pham <cocwedc@gmail.com>
2019-02-08 21:46:49 -0600

    reducers mutate rule

---
+++ src/apis/reducersRule.js
@@ -0,0 +1,25 @@
+let state;
+
+// Removing an element from an array
+state.pop() // bad
+state.filter(element => element != "bar") // good
+
+// Adding an element to an array
+state.push("foo") // bad
+[...state, "bar"] // good
+
+// Replacing an element in  an array
+state[2] = "foo" // bad
+state.map(el => el === 'foo' ? 'bar' : el) // good
+
+// Updating a property in an object
+state.name = 'foo' // bad
+{...state, name: 'bar'} // good
+
+// Adding a property to an object
+state.age = 23 // bad
+{...state, age: 23} // good
+
+// Removing a property from an object
+delete state.name // bad
+{...state, age: undefined} //good
